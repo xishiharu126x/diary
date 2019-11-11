a93c0040f0438bf589016810300620dec041d337
xishiharu126x <ishiharu126@gmail.com>
2019-11-11 11:34:09 +0800

    データベース機能設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema; #追加
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //追加
+        Schema::defaultStringLength(191);
     }
 
     /**
