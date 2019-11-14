7ff7f15cab96da2dcd277abc23a5e8805769f3a4
xishiharu126x <ishiharu126@gmail.com>
2019-11-14 11:50:01 +0800

    layoutの追加
    layoutで上のHTMLとかを一つにまとめた

--- resources/views/diaries/index.blade.php
+++ resources/views/diaries/index.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
-    <title>一覧表示画面</title>
-</head>
-<body>
+
+<!-- layout.blade.phpを読み込む -->
+@extends('layout')
+
+@section('title','一覧')
+
+@section('content')
 <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block mt-3">NEW</a>
   @foreach($diaries as $diary)
     <div class="m-4 p-4 border border-primary">
@@ -23,5 +20,5 @@
 
     </div>
   @endforeach
-</body>
-</html>
+
+@endsection
